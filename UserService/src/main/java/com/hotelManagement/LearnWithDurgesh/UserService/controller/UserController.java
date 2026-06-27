8abb35a7c8ce6ea281f8329876eda5140f99281b
vishal-raut18 <rautvishal70528@gmail.com>
2026-06-27 19:34:01 +0530

    Implemented Fault Tolerance in that Circuit Breaker

--- UserService/src/main/java/com/hotelManagement/LearnWithDurgesh/UserService/controller/UserController.java
+++ UserService/src/main/java/com/hotelManagement/LearnWithDurgesh/UserService/controller/UserController.java
@@ -4,6 +4,9 @@ package com.hotelManagement.LearnWithDurgesh.UserService.controller;
 import com.hotelManagement.LearnWithDurgesh.UserService.entity.User;
 import com.hotelManagement.LearnWithDurgesh.UserService.services.UserServices;
 import com.hotelManagement.LearnWithDurgesh.UserService.services.imp.UserServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,8 @@ public class UserController {
     @Autowired
     private UserServices userService;
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(UserController.class);
     // add
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
@@ -34,8 +39,24 @@ public class UserController {
 
     // get user by id
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserById(@PathVariable String userId) {
         User user1 = userService.getUser(userId);
         return ResponseEntity.status(HttpStatus.OK).body(user1);
     }
+
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+
+        logger.info("Fallback is executed becouse service is down : " + ex.getMessage());
+
+        User  user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created dummy becouse some service is down")
+                .userId("11111111")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+
+    }
+
 }
